a51baf64e90c03f2142bbca94b3f7de1d6cc58b5
michaelsatumba <mjsatumba@gmail.com>
2023-05-31 08:22:01 -0700

    github co-pilot optimized

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import time
 
 xOffset = 100
 
-while True:
+for i in range(10):
     pyautogui.moveRel(xOffset, 0, duration=1)
-    print('Mouse moved')
     xOffset = -xOffset  # toggle between positive and negative values
     time.sleep(10)
